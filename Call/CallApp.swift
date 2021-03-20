07326cdc0fcde35441c1752d33c4bca432dcad37
itnams <68367990+itnams@users.noreply.github.com>
2021-03-20 08:44:33 +0700

    Initial Commit

---
+++ Call/CallApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallApp.swift
+//  Call
+//
+//  Created by namnguyen on 20/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct CallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
